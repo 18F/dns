895be8d5a40f288a32289a5b692ad40645b67c8f
Jez Humble <jez.humble@gsa.gov>
2016-07-08 18:12:24 -0700

    added another couple of pif records

--- terraform/pif.gov.tf
+++ terraform/pif.gov.tf
@@ -20,7 +20,7 @@ resource "aws_route53_record" "www" {
   alias {
     name = "s3-website-us-east-1.amazonaws.com."
     zone_id = "Z3AQBSTGFYJSTF"
-    evaluate_target_health = true
+    evaluate_target_health = false
   }
 }
 
@@ -30,6 +30,22 @@ resource "aws_route53_record" "mx" {
   type = "MX"
   ttl = 60
   records = ["10 alt3.aspmx.l.google.com.", "10 alt4.aspmx.l.google.com.", "1 aspmx.l.google.com.", "5 alt1.aspmx.l.google.com.", "5 alt2.aspmx.l.google.com."]
+}
+
+resource "aws_route53_record" "txt" {
+  zone_id = "${aws_route53_zone.pif_toplevel.zone_id}"
+  name = "pif.gov."
+  type = "TXT"
+  ttl = 60
+  records = ["v=spf1 include:gsa.gov ~all"]
+}
+
+resource "aws_route53_record" "asterisk_cname" {
+  zone_id = "${aws_route53_zone.pif_toplevel.zone_id}"
+  name = "*.pif.gov."
+  type = "CNAME"
+  ttl = 300
+  records = ["cloud-gov-pif-ELB-9ZR5WLEVW83I-466945119.us-east-1.elb.amazonaws.com"]
 }
 
 output "pif_ns" {
