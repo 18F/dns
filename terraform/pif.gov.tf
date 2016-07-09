c7645b6626724e21838400090bd3de070cdc78c4
Jez Humble <jez.humble@gsa.gov>
2016-07-08 18:05:54 -0700

    Added A and MX records

--- terraform/pif.gov.tf
+++ terraform/pif.gov.tf
@@ -12,6 +12,26 @@ resource "aws_route53_zone" "pif_toplevel" {
    }
 }
 
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.pif_toplevel.zone_id}"
+  name = "pif.gov."
+  type = "A"
+
+  alias {
+    name = "s3-website-us-east-1.amazonaws.com."
+    zone_id = "Z3AQBSTGFYJSTF"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "mx" {
+  zone_id = "${aws_route53_zone.pif_toplevel.zone_id}"
+  name = "pif.gov."
+  type = "MX"
+  ttl = 60
+  records = ["10 alt3.aspmx.l.google.com.", "10 alt4.aspmx.l.google.com.", "1 aspmx.l.google.com.", "5 alt1.aspmx.l.google.com.", "5 alt2.aspmx.l.google.com."]
+}
+
 output "pif_ns" {
     value="${aws_route53_zone.pif_toplevel.name_servers}"
 }
