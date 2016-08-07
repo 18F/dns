fd9c2299a564b65d86c027a3696bc64e9140f4ea
Jez Humble <jez.humble@gsa.gov>
2016-08-07 06:31:49 -0700

    Added empty DNS zone for code.gov

--- terraform/code.gov.tf
+++ terraform/code.gov.tf
@@ -8,26 +8,6 @@ resource "aws_route53_zone" "code_toplevel" {
    }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.code_toplevel.zone_id}"
-  name = "www.code.gov."
-  type = "CNAME"
-  ttl = 60
-  records = ["d3qovtf6opsrko.cloudfront.net."]
-}
-
-resource "aws_route53_record" "apex" {
-  zone_id = "${aws_route53_zone.code_toplevel.zone_id}"
-  name = "code.gov."
-  type = "A"
-
-  alias {
-    name = "www.code.gov"
-    zone_id = "${aws_route53_zone.code_toplevel.zone_id}"
-    evaluate_target_health = false
-  }
-}
-
 output "code_ns" {
     value="${aws_route53_zone.code_toplevel.name_servers}"
 }
