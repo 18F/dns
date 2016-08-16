64befdbf004115c58e2f6ae74afb55e77d9a9846
Jez Humble <jez.humble@gsa.gov>
2016-08-16 12:23:05 -0700

    Added empty placeholder zone file for 18f.us

---
+++ terraform/18f.us.tf
@@ -0,0 +1,10 @@
+resource "aws_route53_zone" "18f_us_zone" {
+  name = "18f.us."
+  tags {
+    Project = "dns"
+  }
+}
+
+output "18f_us_ns" {
+  value="${aws_route53_zone.18f_us_zone.name_servers}"
+}
