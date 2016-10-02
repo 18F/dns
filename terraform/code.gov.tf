c79764274c2501ce69582c3268f9270de462b808
Jez Humble <jez@jezhumble.net>
2016-10-02 12:27:11 +0100

    Updated terraform with actual buckets for connect.gov redirects

--- terraform/code.gov.tf
+++ terraform/code.gov.tf
@@ -1,5 +1,3 @@
-variable "env_name" {}
-
 resource "aws_route53_zone" "code_toplevel" {
   name = "code.gov"
 
--- terraform/connect.gov.tf
+++ terraform/connect.gov.tf
@@ -5,11 +5,19 @@ resource "aws_route53_zone" "connect_gov_zone" {
   }
 }
 
-resource "aws_s3_bucket" "connect_gov_redirect" {
-    bucket = "connect-gov"
+resource "aws_s3_bucket" "connect_gov_redirect_root" {
+    bucket = "connect.gov"
     acl = "public-read"
     website {
       redirect_all_requests_to = "https://login.gov/"
+    }
+}
+
+resource "aws_s3_bucket" "connect_gov_redirect_www" {
+    bucket = "www.connect.gov"
+    acl = "public-read"
+    website {
+      redirect_all_requests_to = "https://www.login.gov/"
     }
 }
 
@@ -19,8 +27,8 @@ resource "aws_route53_record" "connect_gov_connect_gov_a" {
   type = "A"
 
   alias {
-    name = "${aws_s3_bucket.connect_gov_redirect.website_domain}"
-    zone_id = "${aws_s3_bucket.connect_gov_redirect.hosted_zone_id}"
+    name = "${aws_s3_bucket.connect_gov_redirect_root.website_domain}"
+    zone_id = "${aws_s3_bucket.connect_gov_redirect_root.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
@@ -30,8 +38,8 @@ resource "aws_route53_record" "www_connect_gov_connect_gov_cname" {
   name = "www.connect.gov"
   type = "A"
   alias {
-    name = "${aws_s3_bucket.connect_gov_redirect.website_domain}"
-    zone_id = "${aws_s3_bucket.connect_gov_redirect.hosted_zone_id}"
+    name = "${aws_s3_bucket.connect_gov_redirect_www.website_domain}"
+    zone_id = "${aws_s3_bucket.connect_gov_redirect_www.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
@@ -56,11 +64,10 @@ resource "aws_route53_record" "v_connect_gov_txt" {
   records = ["v=spf1 include:spf_sa.gsa.gov ~all"]
 }
 
-resource "aws_route53_record" "google_site_verification_connect_gov_txt" {
+resource "aws_route53_record" "google_site_connect_gov_txt" {
   zone_id = "${aws_route53_zone.connect_gov_zone.zone_id}"
   name = "connect.gov"
   type = "TXT"
   ttl = 600
   records = ["google-site-verification=j3qyXzcDt_O3t0sdYy6FCQlYJnV5ASd0GYIhicPPzOg"]
 }
-
