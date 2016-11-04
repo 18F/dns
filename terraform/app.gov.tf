c5c79e38156cf046ee2423fb98dd40a63aacba75
Jez Humble <jez.humble@gsa.gov>
2016-11-03 17:37:58 -0700

    Added redirect for app.gov and related S3 buckets

--- terraform/app.gov.tf
+++ terraform/app.gov.tf
@@ -5,13 +5,29 @@ resource "aws_route53_zone" "app_gov_zone" {
   }
 }
 
+resource "aws_s3_bucket" "app_gov_redirect_root" {
+    bucket = "app.gov"
+    acl = "public-read"
+    website {
+      redirect_all_requests_to = "https://apps.gov/"
+    }
+}
+
+resource "aws_s3_bucket" "app_gov_redirect_www" {
+    bucket = "www.app.gov"
+    acl = "public-read"
+    website {
+      redirect_all_requests_to = "https://www.apps.gov/"
+    }
+}
+
 resource "aws_route53_record" "app_gov_app_gov_a" {
   zone_id = "${aws_route53_zone.app_gov_zone.zone_id}"
   name = "app.gov."
   type = "A"
   alias {
-    name = "s3-website-us-east-1.amazonaws.com."
-    zone_id = "Z3AQBSTGFYJSTF"
+    name = "${aws_s3_bucket.app_gov_redirect_root.website_domain}"
+    zone_id = "${aws_s3_bucket.app_gov_redirect_root.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
@@ -21,8 +37,8 @@ resource "aws_route53_record" "app_gov_www_app_gov_a" {
   name = "www.app.gov."
   type = "A"
   alias {
-    name = "s3-website-us-east-1.amazonaws.com."
-    zone_id = "Z3AQBSTGFYJSTF"
+    name = "${aws_s3_bucket.app_gov_redirect_www.website_domain}"
+    zone_id = "${aws_s3_bucket.app_gov_redirect_www.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
