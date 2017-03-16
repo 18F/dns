39b2c5baf01e2117e0d5ee9ef970816a4caaab54
Julia Elman <julia.elman@gsa.gov>
2017-03-16 13:53:21 -0400

    Adding standards.usa.gov to 18F/dns.

--- terraform/usa.gov.tf
+++ terraform/usa.gov.tf
@@ -27,6 +27,28 @@ resource "aws_route53_record" "usa_gov_analytics_usa_gov_aaaa" {
   }
 }
 
+resource "aws_route53_record" "usa_gov_standards_usa_gov_a" {
+  zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
+  name = "standards.usa.gov."
+  type = "A"
+  alias {
+    name = "d98p4kkshmqgd.cloudfront.net."
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "usa_gov_standards_usa_gov_aaaa" {
+  zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
+  name = "standards.usa.gov."
+  type = "AAAA"
+  alias {
+    name = "d98p4kkshmqgd.cloudfront.net."
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
 output "usa_gov_ns" {
   value="${aws_route53_zone.usa_gov_zone.name_servers}"
 }
