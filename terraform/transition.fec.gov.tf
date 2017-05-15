81c9c7cf37a65678a1d62b0e7492f4ff21b15be5
LindsayYoung <lindsayn.young@gsa.gov>
2017-05-15 16:20:05 -0400

    Adding cname for fec.gov

--- terraform/transition.fec.gov.tf
+++ terraform/transition.fec.gov.tf
@@ -1,16 +1,20 @@
-resource "aws_route53_zone" "transition_gov_us_zone" {
-  name = "transition.fec.gov"
-  tags {
-    Project = "dns"
-  }
-}
-
 resource "aws_route53_record" "transition_gov_transition_gov_cname" {
   zone_id = "${aws_route53_zone.transition_gov_us_zone.zone_id}"
   name = "transition.fec.gov"
   type = "A"
   alias {
     name = "d2p6ccc3xlipxg.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_fec_gov_cname" {
+  zone_id = "${aws_route53_zone.www_fec_gov_us_zone.zone_id}"
+  name = "www.fec.gov"
+  type = "CNAME"
+  alias {
+    name = "d3t5a36r5g3qx4.cloudfront.net"
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
