142bae1ffbab0ba604da0136268cababe426f606
Steven Reilly <stvnrlly@users.noreply.github.com>
2017-05-24 15:58:17 -0400

    add new apps.gov cloudfront distribution

--- terraform/apps.gov.tf
+++ terraform/apps.gov.tf
@@ -10,7 +10,7 @@ resource "aws_route53_record" "apps_gov_apps_gov_a" {
   name = "apps.gov."
   type = "A"
   alias {
-    name = "d37tzfgd0a69in.cloudfront.net."
+    name = "d24f99alwtdu0h.cloudfront.net."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
