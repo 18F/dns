f5d8478806b8140e99a1811e29bf8b9ce690817f
Jeremy Zilar <jeremy.zilar@gsa.gov>
2017-08-18 16:33:24 -0400

    Creating a new terraform file for digital.gov
    I believe that the only thing left to add to this is the `zone_id = "XXXXXXX"`

---
+++ terraform/digital.gov.tf
@@ -0,0 +1,47 @@
+resource "aws_route53_zone" "digital_toplevel" {
+  name = "digital.gov"
+
+  tags {
+    Project = "dns"
+  }
+}
+
+resource "aws_route53_record" "digital_gov_apex" {
+  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
+  name = "digital.gov."
+  type = "A"
+
+  alias {
+    name = "d2q1i25any8vwy.cloudfront.net."
+    zone_id = "XXXXXXX"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "digital_gov_www" {
+  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
+  name = "www.digital.gov."
+  type = "A"
+
+  alias {
+    name = "d2q1i25any8vwy.cloudfront.net."
+    zone_id = "XXXXXXX"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "demo_digital_gov_a" {
+  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
+  name = "demo.code.gov."
+  type = "A"
+
+  alias {
+    name = "d1f2igtqmwwbgm.cloudfront.net."
+    zone_id = "XXXXXXX"
+    evaluate_target_health = false
+  }
+}
+
+output "code_ns" {
+  value="${aws_route53_zone.digital_toplevel.name_servers}"
+}
