3f3634d333652a849d33dd5332cd785abd654482
Jeremy Zilar <jeremy.zilar@gsa.gov>
2017-09-20 21:07:04 -0400

    updating the CloudFront URLs
    and fixing the innnnnnovation typo

--- terraform/innovation.gov.tf
+++ terraform/innovation.gov.tf
@@ -1,47 +1,47 @@
-resource "aws_route53_zone" "innnovation_toplevel" {
-  name = "innnovation.gov"
+resource "aws_route53_zone" "innovation_toplevel" {
+  name = "innovation.gov"
 
   tags {
     Project = "dns"
   }
 }
 
-resource "aws_route53_record" "innnovation_gov_apex" {
-  zone_id = "${aws_route53_zone.innnovation_toplevel.zone_id}"
-  name = "innnovation.gov."
+resource "aws_route53_record" "innovation_gov_apex" {
+  zone_id = "${aws_route53_zone.innovation_toplevel.zone_id}"
+  name = "innovation.gov."
   type = "A"
 
   alias {
-    name = "d2q1i25any8vwy.cloudfront.net."
+    name = "d2ntl68ywjm643.cloudfront.net."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "innnovation_gov_www" {
-  zone_id = "${aws_route53_zone.innnovation_toplevel.zone_id}"
-  name = "www.innnovation.gov."
+resource "aws_route53_record" "innovation_gov_www" {
+  zone_id = "${aws_route53_zone.innovation_toplevel.zone_id}"
+  name = "www.innovation.gov."
   type = "A"
 
   alias {
-    name = "d2q1i25any8vwy.cloudfront.net."
+    name = "d2ntl68ywjm643.cloudfront.net."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "demo_innnovation_gov_a" {
-  zone_id = "${aws_route53_zone.innnovation_toplevel.zone_id}"
-  name = "demo.innnovation.gov."
+resource "aws_route53_record" "demo_innovation_gov_a" {
+  zone_id = "${aws_route53_zone.innovation_toplevel.zone_id}"
+  name = "demo.innovation.gov."
   type = "A"
 
   alias {
-    name = "d1f2igtqmwwbgm.cloudfront.net."
+    name = "d3am9l7wwd0yie.cloudfront.net."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
 
-output "innnovation_ns" {
-  value="${aws_route53_zone.innnovation_toplevel.name_servers}"
+output "innovation_ns" {
+  value="${aws_route53_zone.innovation_toplevel.name_servers}"
 }
