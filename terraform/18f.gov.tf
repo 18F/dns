098113abc6c360d4440ce8bcf0c4b57cabb2f897
Erik Burgess <erik.burgess@gsa.gov>
2018-01-08 09:35:34 -0500

    Update 18f.gov.tf
    Added DMARC Reject Policy per GSA request.

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -32,6 +32,16 @@ resource "aws_route53_record" "18f_gov_18f_gov_txt" {
   records = ["v=spf1 include:spf.mandrillapp.com ?all"]
 }
 
+resource "aws_route53_record" "18f_gov__dmarc_18f_gov_txt" {
+  zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
+  name = "_dmarc.18f.gov."
+  type = "TXT"
+  ttl = 300
+  records = [
+     "v=DMARC1; p=none; pct=10; fo=1; ri=86400; rua=mailto:dmarcreports@gsa.gov,mailto:reports@dmarc.cyber.dhs.gov; ruf=mailto:dmarcfailures@gsa.gov"
+  ]
+}
+
 resource "aws_route53_record" "18f_gov_01326251e4e9fc611488dd6bceaeba90_18f_gov_cname" {
   zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
   name = "01326251e4e9fc611488dd6bceaeba90.18f.gov."
