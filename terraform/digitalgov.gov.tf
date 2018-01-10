b8225e04b2f323af2fe49eca2f690f7a35038b34
Erik Burgess <erik.burgess@gsa.gov>
2018-01-10 16:58:25 -0500

    Revert "Reverting to old DNS for OPM / openopps.digitalgov.gov"

--- terraform/digitalgov.gov.tf
+++ terraform/digitalgov.gov.tf
@@ -86,58 +86,16 @@ resource "aws_route53_record" "demo_digitalgov_gov_a" {
 }
 
 
-
-# OpenOpps ------------------------------------------
-
-resource "aws_route53_record" "digitalgov_gov_openopps_digitalgov_gov_a" {
+# openopps.digitalgov.gov
+resource "aws_route53_record" "openopps_digitalgov_gov_a" {
   zone_id = "${aws_route53_zone.digitalgov_gov_zone.zone_id}"
   name = "openopps.digitalgov.gov."
-  type = "A"
-  alias {
-    name = "d11og6pgwhrztr.cloudfront.net."
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
+  type = "CNAME"
+  ttl = "300"
+  records = [
+    "openopps.usajobs.gov."
+  ]
 }
-
-resource "aws_route53_record" "digitalgov_gov_openopps_digitalgov_gov_aaaa" {
-  zone_id = "${aws_route53_zone.digitalgov_gov_zone.zone_id}"
-  name = "openopps.digitalgov.gov."
-  type = "AAAA"
-  alias {
-    name = "d11og6pgwhrztr.cloudfront.net."
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "digitalgov_gov_openopps_digitalgov_gov_mx" {
-  zone_id = "${aws_route53_zone.digitalgov_gov_zone.zone_id}"
-  name = "openopps.digitalgov.gov."
-  type = "MX"
-  ttl = 300
-  records = ["10	30288227.in1.mandrillapp.com", "20	30288227.in2.mandrillapp.com"]
-}
-
-resource "aws_route53_record" "digitalgov_gov_openopps_digitalgov_gov_txt" {
-  zone_id = "${aws_route53_zone.digitalgov_gov_zone.zone_id}"
-  name = "openopps.digitalgov.gov."
-  type = "TXT"
-  ttl = 300
-  records = ["v=spf1 include:spf.mandrillapp.com ?all"]
-}
-
-resource "aws_route53_record" "digitalgov_gov_mandrill__domainkey_openopps_digitalgov_gov_txt" {
-  zone_id = "${aws_route53_zone.digitalgov_gov_zone.zone_id}"
-  name = "mandrill._domainkey.openopps.digitalgov.gov."
-  type = "TXT"
-  ttl = 300
-  records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"]
-}
-
-# END OpenOpps ------------------------------------------
-
-
 
 # usdigitalregistry.digitalgov.gov
 resource "aws_route53_record" "usdigitalregistry_digitalgov_gov_a" {
