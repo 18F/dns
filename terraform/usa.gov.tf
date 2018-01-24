719ef673de3aca0f04474a4b03eb81f8244ca73e
Jeremy Zilar <jeremy.zilar@gsa.gov>
2018-01-24 15:54:55 -0500

    Pointing the USWDS domains at the USA.gov redirect server

--- terraform/usa.gov.tf
+++ terraform/usa.gov.tf
@@ -27,45 +27,26 @@ resource "aws_route53_record" "usa_gov_analytics_usa_gov_aaaa" {
   }
 }
 
+# USWDS ------------------------------------------------
+# Pointing at the USA.gov redirect server
 resource "aws_route53_record" "usa_gov_components_standards_usa_gov_a" {
   zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
   name = "components.standards.usa.gov."
   type = "A"
   alias {
-    name = "d1trwt77dwz8ml.cloudfront.net."
+    name = "54.85.132.205."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "usa_gov_components_standards_usa_gov_aaaa" {
-  zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
-  name = "components.standards.usa.gov."
-  type = "AAAA"
-  alias {
-    name = "d1trwt77dwz8ml.cloudfront.net."
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
-
+# Pointing at the USA.gov redirect server
 resource "aws_route53_record" "usa_gov_standards_usa_gov_a" {
   zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
   name = "standards.usa.gov."
   type = "A"
   alias {
-    name = "d98p4kkshmqgd.cloudfront.net."
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "usa_gov_standards_usa_gov_aaaa" {
-  zone_id = "${aws_route53_zone.usa_gov_zone.zone_id}"
-  name = "standards.usa.gov."
-  type = "AAAA"
-  alias {
-    name = "d98p4kkshmqgd.cloudfront.net."
+    name = "54.85.132.205."
     zone_id = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
