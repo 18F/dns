982fa7397e60874dec7dcb28bb4bf97b84b1f441
Erik Burgess <erik.burgess@gsa.gov>
2018-04-20 10:45:10 -0400

    Update 18f.us.tf
    Test push for TF import.

--- terraform/18f.us.tf
+++ terraform/18f.us.tf
@@ -5,6 +5,14 @@ resource "aws_route53_zone" "18f_us_zone" {
   }
 }
 
+resource "aws_route53_record" "18f_us_test_18f_us_cname" {
+  zone_id = "${aws_route53_zone.18f_us_zone.zone_id}"
+  name = "test.18f.us."
+  type = "CNAME"
+  ttl = 300
+  records = ["dualstack.production-cloudfoundry-apps-1374881977.us-gov-west-1.elb.amazonaws.com."]
+}
+
 output "18f_us_ns" {
   value="${aws_route53_zone.18f_us_zone.name_servers}"
 }
