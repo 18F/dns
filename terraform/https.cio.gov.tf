a0ed930d7e40960da85c9bf89c67eceb96f7e89f
Erik Burgess <erik.burgess@gsa.gov>
2018-06-06 15:13:40 -0400

    Add files via upload

---
+++ terraform/https.cio.gov.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_zone" "https_cio_gov_zone" {
+  name = "https.cio.gov."
+
+  tags {
+    Project = "dns"
+  }
+}
+
+resource "aws_route53_record" "https_cio_gov_https_cio_gov_a" {
+  zone_id = "${aws_route53_zone.https_cio_gov_zone.zone_id}"
+  name    = "https.cio.gov."
+  type    = "A"
+
+  alias {
+    name                   = "d2h7trd5jt3vay.cloudfront.net."
+    zone_id                = "${local.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+output "https_cio_gov_ns" {
+  value = "${aws_route53_zone.https_cio_gov_zone.name_servers}"
+}
---
+++ terraform/open.foia.gov.tf
@@ -0,0 +1,31 @@
+resource "aws_route53_zone" "open_foia_gov_zone" {
+  name = "open.foia.gov."
+
+  tags {
+    Project = "dns"
+  }
+}
+
+resource "aws_route53_record" "open_foia_gov_open_foia_gov_a" {
+  zone_id = "${aws_route53_zone.open_foia_gov_zone.zone_id}"
+  name    = "open.foia.gov."
+  type    = "A"
+
+  alias {
+    name                   = "d23kapr45ru7n0.cloudfront.net."
+    zone_id                = "${local.cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "open_foia_gov_17fdb56b3309467b9d43ad74888c07e2_open_foia_gov_cname" {
+  zone_id = "${aws_route53_zone.open_foia_gov_zone.zone_id}"
+  name    = "17fdb56b3309467b9d43ad74888c07e2.open.foia.gov."
+  type    = "CNAME"
+  ttl     = 300
+  records = ["6ca1ebd4ffc4a9400ac70f600b7139bdd92625df.comodoca.com."]
+}
+
+output "open_foia_gov_ns" {
+  value = "${aws_route53_zone.open_foia_gov_zone.name_servers}"
+}
