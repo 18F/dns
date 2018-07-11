7a8a5c65630b43249509a82a61fa97e942ca63da
Will Slack <wslack@users.noreply.github.com>
2018-07-11 19:45:26 -0400

    change blogging guide to CNAME

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -270,14 +270,12 @@ resource "aws_route53_record" "18f_gov_automated-testing-playbook_18f_gov_a" {
   }
 }
 
-resource "aws_route53_record" "18f_gov_blogging-guide_18f_gov_a" {
+resource "aws_route53_record" "18f_gov_blogging-guide_18f_gov_cname" {
   zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
   name = "blogging-guide.18f.gov."
-  type = "A"
-  alias {
-    name = "pages-redirects.app.cloud.gov."
-    zone_id = "${local.cloudfront_zone_id}"
-    evaluate_target_health = false
+  type = "CNAME"
+  ttl = 300
+  records = ["pages-redirects.app.cloud.gov."]
   }
 }
 
