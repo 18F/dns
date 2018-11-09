4bb99b36fc2ee808e4f5c658a508a719b24a294d
Eddie A Tejeda <eddie.tejeda@gsa.gov>
2018-11-09 12:00:04 -0500

    add federalistapp.18f and federalistapp-staging.18f for future URL migration

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -490,9 +490,25 @@ resource "aws_route53_record" "18f_gov_federalist_18f_gov_cname" {
   records = ["d189ghshxys967.cloudfront.net"]
 }
 
+resource "aws_route53_record" "18f_gov_federalistapp_18f_gov_cname" {
+  zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
+  name = "federalistapp.18f.gov."
+  type = "CNAME"
+  ttl = 60
+  records = ["d189ghshxys967.cloudfront.net"]
+}
+
 resource "aws_route53_record" "18f_gov_federalist-staging_18f_gov_cname" {
   zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
   name = "federalist-staging.18f.gov."
+  type = "CNAME"
+  ttl = 60
+  records = ["d3e7le7trh4ed5.cloudfront.net"]
+}
+
+resource "aws_route53_record" "18f_gov_federalistapp-staging_18f_gov_cname" {
+  zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
+  name = "federalistapp-staging.18f.gov."
   type = "CNAME"
   ttl = 60
   records = ["d3e7le7trh4ed5.cloudfront.net"]
