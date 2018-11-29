5bc7fc49e34d655c499d4578e19813f27be49427
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-11-29 16:43:35 -0500

    re-add federalist-staging

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -506,6 +506,14 @@ resource "aws_route53_record" "18f_gov_federalistapp_18f_gov_cname" {
   records = ["d189ghshxys967.cloudfront.net"]
 }
 
+resource "aws_route53_record" "18f_gov_federalist-staging_18f_gov_cname" {
+  zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
+  name = "federalist-staging.18f.gov."
+  type = "CNAME"
+  ttl = 60
+  records = ["d1gq90wql59r7i.cloudfront.net"]
+}
+
 resource "aws_route53_record" "18f_gov_federalistapp-staging_18f_gov_cname" {
   zone_id = "${aws_route53_zone.18f_gov_zone.zone_id}"
   name = "federalistapp-staging.18f.gov."
