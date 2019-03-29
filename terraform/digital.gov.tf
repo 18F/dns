2976a419ac6e4a131aba8bf4ec617518b13a429f
Jeremy Zilar <jeremyzilar@gmail.com>
2019-03-29 12:06:34 -0400

    updating zone id

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -113,7 +113,7 @@ resource "aws_route53_record" "public_sans_digital_gov_aaaa" {
   type = "A"
   alias {
     name = "d3t8f8c09txt9n.cloudfront.net."
-    zone_id = "${local.cloudfront_zone_id}"
+    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
     evaluate_target_health = false
   }
 }
