b36831043a7bb6cf14b6fb4794e776271928d187
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-04-01 10:56:15 -0400

    add public-sans cloudfront

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -106,6 +106,18 @@ resource "aws_route53_record" "components_designsystem_digital_gov_aaaa" {
   }
 }
 
+# public-sans.digital.gov — A
+resource "aws_route53_record" "public_sans_digital_gov_aaaa" {
+  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
+  name = "public-sans.digital.gov."
+  type = "A"
+  alias {
+    name = "d30jruftdogur6.cloudfront.net."
+    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 # v2.designsystem.digital.gov — A -------------------------------
 resource "aws_route53_record" "v2_designsystem_digital_gov_a" {
   zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
