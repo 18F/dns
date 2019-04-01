606f755bcf33dcd646548043375b28000de20292
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-04-01 11:03:55 -0400

    local.cloudfront_zone_id for public-sans zone

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -113,7 +113,7 @@ resource "aws_route53_record" "public_sans_digital_gov_a" {
   type = "A"
   alias {
     name = "d30jruftdogur6.cloudfront.net."
-    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
+    zone_id = "${local.cloudfront_zone_id}"
     evaluate_target_health = false
   }
 }
