d71e6d6165551859e17aee8b50b6641d6526f0ab
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-04-01 10:41:14 -0400

    temp remove public sans

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -106,18 +106,6 @@ resource "aws_route53_record" "components_designsystem_digital_gov_aaaa" {
   }
 }
 
-# public-sans.digital.gov — A
-resource "aws_route53_record" "public_sans_digital_gov_aaaa" {
-  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
-  name = "public-sans.digital.gov."
-  type = "A"
-  alias {
-    name = "d3t8f8c09txt9n.cloudfront.net."
-    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
-    evaluate_target_health = false
-  }
-}
-
 # v2.designsystem.digital.gov — A -------------------------------
 resource "aws_route53_record" "v2_designsystem_digital_gov_a" {
   zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
