9806745afaf8e9d1ce4f1448638b6a1e6e01e695
Jeremy Zilar <jeremyzilar@gmail.com>
2019-04-09 21:12:50 -0400

    adding in demo.designsystem

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -119,6 +119,18 @@ resource "aws_route53_record" "public_sans_digital_gov_a" {
 }
 
 
+# demo.designsystem.digital.gov — A -------------------------------
+resource "aws_route53_record" "demo_designsystem_digital_gov_a" {
+  zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
+  name = "demo.designsystem.digital.gov."
+  type = "A"
+  alias {
+    name = "xxxxxxxxxxxxx.cloudfront.net."
+    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
 # v1.designsystem.digital.gov — A -------------------------------
 resource "aws_route53_record" "v1_designsystem_digital_gov_a" {
   zone_id = "${aws_route53_zone.digital_toplevel.zone_id}"
