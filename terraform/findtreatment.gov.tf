97dadcc7862feb82711644fb05829cc6b52d9cfe
Brian Hurst <brian.hurst@gsa.gov>
2019-09-24 14:52:19 -0400

    Update findtreatment.gov.tf

--- terraform/findtreatment.gov.tf
+++ terraform/findtreatment.gov.tf
@@ -6,30 +6,6 @@ resource "aws_route53_zone" "findtreatment_toplevel" {
   }
 }
 
-resource "aws_route53_record" "findtreatment_apex" {
-  zone_id = "${aws_route53_zone.findtreatment_toplevel.zone_id}"
-  name = "findtreatment.gov."
-  type = "A"
-
-  alias {
-    name = "d1qy5q7pncs690.cloudfront.net."
-    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "findtreatment_www" {
-  zone_id = "${aws_route53_zone.findtreatment_toplevel.zone_id}"
-  name = "www.findtreatment.gov."
-  type = "A"
-
-  alias {
-    name = "d1qy5q7pncs690.cloudfront.net."
-    zone_id = "${local.cloud_gov_cloudfront_zone_id}"
-    evaluate_target_health = false
-  }
-}
-
 # BOD
 resource "aws_route53_record" "findtreatment_gov__dmarc_findtreatment_gov_txt" {
   zone_id = "${aws_route53_zone.findtreatment_toplevel.zone_id}"
