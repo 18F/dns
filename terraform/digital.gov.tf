cc0452ca70a177aef4de49437785c4cd26c1ea82
Jeremy Zilar <jeremy.zilar@gsa.gov>
2020-01-21 15:53:14 -0500

    adding the local.spf_hubspot variable

--- terraform/digital.gov.tf
+++ terraform/digital.gov.tf
@@ -381,9 +381,7 @@ resource "aws_route53_record" "digital_gov_dmarc_digital_gov_txt" {
   name    = "digital.gov."
   type    = "TXT"
   ttl     = 300
-  records = [
-    "v=spf1 include:1962994.spf05.hubspotemail.net ~all"
-  ]
+  records = ["${local.spf_hubspot}"]
 }
 
 # v2.designsystem.digital.gov TXT / ACME Challenge
