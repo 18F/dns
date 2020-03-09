87fcdb00a5edcd5c445d5b87dcb97dc5feb9679f
Aidan Feldman <aidan.feldman@gsa.gov>
2020-03-09 03:16:18 -0400

    restrict Terraform and AWS provider versions

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  version = "~> 2.52"
 }
 
 terraform {
+  required_version = "~> 0.11.0"
   backend "s3" {
     region = "us-east-1"
   }
