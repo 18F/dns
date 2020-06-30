f7f21c224ba07ae8cdad4e8119e2b1375d611f03
Aidan Feldman <aidan.feldman@gsa.gov>
2020-06-30 18:05:36 -0400

    use a wildcard for usability.gov's SPF record

--- terraform/usability.gov.tf
+++ terraform/usability.gov.tf
@@ -42,13 +42,13 @@ resource "aws_route53_record" "usability_gov__spf" {
   records = ["${local.spf_no_mail}"]
 }
 
-# resource "aws_route53_record" "usability_gov__www_spf" {
-#   zone_id = "${aws_route53_zone.usability_toplevel.zone_id}"
-#   name = "www.usability.gov."
-#   type = "TXT"
-#   ttl = 300
-#   records = ["${local.spf_no_mail}"]
-# }
+resource "aws_route53_record" "usability_gov__wildcard_spf" {
+  zone_id = "${aws_route53_zone.usability_toplevel.zone_id}"
+  name = "*.usability.gov."
+  type = "TXT"
+  ttl = 300
+  records = ["${local.spf_no_mail}"]
+}
 
 # BOD / DMARC
 resource "aws_route53_record" "usability_gov__dmarc_usability_gov_txt" {
