4ee8708ae8c76a5f019176a635b8377566153e07
Aidan Feldman <aidan.feldman@gsa.gov>
2020-10-06 02:13:27 -0400

    get rid of symlinks to avoid confusion

--- terraform/citizenscience.gov.tf
+++ terraform/citizenscience.gov.tf
@@ -1,1 +1,2 @@
-challenge.gov.tf
+# managed by Corporate IT - request changes through
+# https://gsa.servicenowservices.com/sp/?id=kb_article&sys_id=075cd185db26578058c2fd721f96193b
--- terraform/identitysandbox.gov.tf
+++ terraform/identitysandbox.gov.tf
@@ -1,1 +1,2 @@
-login.gov.tf
+# login.gov DNS configuration is managed separately:
+# https://github.com/18F/identity-devops/blob/master/doc/structure-of-this-repository.md#terraform-dns-directory
