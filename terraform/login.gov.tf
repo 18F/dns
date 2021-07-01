da606937b89317abbf36e31bb8b74511c42ea97c
Aaron D Borden <aaron.borden@gsa.gov>
2021-07-01 09:27:13 -0700

    Restore login.gov breadcrumbs
    Leave these files in as breadcrumbs for folks to find the private login.gov DNS
    repo. Update the broken links so they don't 404 for those with access.

---
+++ terraform/login.gov.tf
@@ -0,0 +1,2 @@
+# login.gov DNS configuration is managed separately in a private repo:
+# https://github.com/18F/identity-devops/blob/main/doc/structure-of-this-repository.md#terraform-dns-directory
