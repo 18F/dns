289cfd7d15b0ed635b975de66cfc83b9d09f14bb
Ryan Palmer <80478507+rpalmer-gsa@users.noreply.github.com>
2022-01-12 17:30:17 -0500

    Create covidtest.usa.gov.tf
    Create hosted zone for and covidtest.usa.gov and staging-covidtest.usa.gov. Create cname to point to www.usa.gov.

---
+++ terraform/covidtest.usa.gov.tf
@@ -0,0 +1,39 @@
+resource "aws_route53_zone" "covidtest_usa_gov_zone" {
+  name = "covidtest.usa.gov."
+
+  tags = {
+    Project = "dns"
+  }
+}
+
+resource "aws_route53_zone" "staging_covidtest_usa_gov_zone" {
+  name = "staging-covidtest.usa.gov."
+
+  tags = {
+    Project = "dns"
+  }
+}
+
+resource "aws_route53_record" "covidtest_usa_gov_cname" {
+  zone_id = aws_route53_zone.covidtest_usa_gov_zone.zone_id
+  name    = "covidtest.usa.gov."
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["www.usa.gov"]
+}
+
+resource "aws_route53_record" "staging_covidtest_usa_gov_cname" {
+  zone_id = aws_route53_zone.covidtest_usa_gov_zone.zone_id
+  name    = "staging-covidtest.usa.gov."
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["www.usa.gov"]
+}
+
+output "covidtest_usa_gov_ns" {
+  value = aws_route53_zone.covidtest_usa_gov_zone.name_servers
+}
+
+output "staging_covidtest_usa_gov_ns" {
+  value = aws_route53_zone.staging_covidtest_usa_gov_zone.name_servers
+}
