2b2dd51ce5cedaf3e831e7bbc650b05fc22ee124
Ryan Palmer <80478507+rpalmer-gsa@users.noreply.github.com>
2022-01-12 21:19:55 -0500

    Only name servers needed at this point in time.

--- terraform/covidtest.usa.gov.tf
+++ terraform/covidtest.usa.gov.tf
@@ -6,20 +6,21 @@ resource "aws_route53_zone" "covidtest_usa_gov_zone" {
   }
 }
 
-resource "aws_route53_record" "covidtest_usa_gov_cname" {
-  zone_id = aws_route53_zone.covidtest_usa_gov_zone.zone_id
-  name    = "covidtest.usa.gov."
-  type    = "CNAME"
-  ttl     = "60"
-  records = ["www.usa.gov"]
-}
-
 resource "aws_route53_zone" "staging_covidtest_usa_gov_zone" {
   name = "staging-covidtest.usa.gov."
 
   tags = {
     Project = "dns"
   }
+}
+
+/* these aren't needed at this point in time. 
+resource "aws_route53_record" "covidtest_usa_gov_cname" {
+  zone_id = aws_route53_zone.covidtest_usa_gov_zone.zone_id
+  name    = "covidtest.usa.gov."
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["www.usa.gov"]
 }
 
 resource "aws_route53_record" "staging_covidtest_usa_gov_cname" {
@@ -29,6 +30,8 @@ resource "aws_route53_record" "staging_covidtest_usa_gov_cname" {
   ttl     = "60"
   records = ["www.usa.gov"]
 }
+*/
+
 
 output "covidtest_usa_gov_ns" {
   value = aws_route53_zone.covidtest_usa_gov_zone.name_servers
