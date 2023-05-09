17f102b0b823c2a96db1620d979be5d827815451
Ryan Ahearn <ryan.ahearn@gsa.gov>
2023-05-09 14:50:13 -0400

    Add beta.notify.gov records for cloud.gov

--- terraform/notify.gov.tf
+++ terraform/notify.gov.tf
@@ -6,6 +6,24 @@ resource "aws_route53_zone" "notify_gov_zone" {
     }
 }
 
+resource "aws_route53_record" "notify_gov_beta_acmechallenge" {
+    zone_id = aws_route53_zone.notify_gov_zone.zone_id
+    name = "_acme-challenge.beta"
+    type = "CNAME"
+
+    ttl = 600
+    records = ["_acme-challenge.beta.notify.gov.external-domains-production.cloud.gov"]
+}
+
+resource "aws_route53_record" "notify_gov_beta_cname" {
+    zone_id = aws_route53_zone.notify_gov_zone.zone_id
+    name = "beta"
+    type = "CNAME"
+
+    ttl = 600
+    records = ["beta.notify.gov.external-domains-production.cloud.gov"]
+}
+
 output "notify_gov_ns" {
     value = aws_route53_zone.notify_gov_zone.name_servers
 }
