d1ed29c9eb93c717957d68bee9b2a334bec850b0
Ryan Ahearn <ryan.ahearn@gsa.gov>
2023-05-10 09:46:42 -0400

    Add NS and DS records for SSB-managed zone

--- terraform/notify.gov.tf
+++ terraform/notify.gov.tf
@@ -78,6 +78,29 @@ resource "aws_route53_record" "notify_gov_mx" {
     records = ["10 feedback-smtp.us-gov-west-1.amazonses.com"]
 }
 
+resource "aws_route53_record" "notify_gov_ssb_ns" {
+    zone_id = aws_route53_zone.notify_gov_zone.zone_id
+    name = "ssb"
+    type = "NS"
+
+    ttl = 600
+    records = [
+        "ns-1030.awsdns-00.org",
+        "ns-1907.awsdns-46.co.uk",
+        "ns-71.awsdns-08.com",
+        "ns-851.awsdns-42.net"
+    ]
+}
+
+resource "aws_route53_record" "notify_gov_ssb_ds" {
+    zone_id = aws_route53_zone.notify_gov_zone.zone_id
+    name = "ssb"
+    type = "DS"
+
+    ttl = 600
+    records = ["62629 13 2 2626E4C8594EA7F41B0F8C471FA50F9334A33F8E4EC17FD38556D90EB926163E"]
+}
+
 output "notify_gov_ns" {
     value = aws_route53_zone.notify_gov_zone.name_servers
 }
