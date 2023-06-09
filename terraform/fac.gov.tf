f90c61cde84455cc2fee5d8a098019c04795f437
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2023-06-09 11:41:59 -0700

    Add api.fac.gov CNAME
    This adds the CNAME for api.fac.gov -> api.data.gov.

--- terraform/fac.gov.tf
+++ terraform/fac.gov.tf
@@ -78,6 +78,15 @@ resource "aws_route53_record" "fac_gov__app_fac_gov_cname" {
   records = ["www.fac.gov.external-domains-production.cloud.gov."]
 }
 
+# CNAME for api.fac.gov
+resource "aws_route53_record" "fac_gov__api_fac_gov_cname" {
+  zone_id = aws_route53_zone.fac_gov_zone.zone_id
+  name    = "api.fac.gov."
+  type    = "CNAME"
+  ttl     = 60
+  records = ["api.data.gov."]
+}
+
 # SPF for mail from fac.gov
 module "fac_gov__email_security" {
   source = "./email_security"
