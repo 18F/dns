a7acb5cc14dc5a787f2f674aca0cba215f7f834a
Paul Hirsch <paul.hirsch@gsa.gov>
2023-09-05 10:28:53 -0500

    Prepare tts.gsa.gov zone

--- terraform/tts.gsa.gov.tf
+++ terraform/tts.gsa.gov.tf
@@ -6,6 +6,16 @@ resource "aws_route53_zone" "tts_gsa_gov_zone" {
   }
 }
 
+# ACME challenge for tts.gsa.gov
+resource "aws_route53_record" "_acme_challenge_tts_gsa_gov_cname" {
+  zone_id = aws_route53_zone.tts_gsa_gov_zone.zone_id
+  name    = "_acme-challenge.tts.gsa.gov."
+  type    = "CNAME"
+  ttl     = 60
+  records = ["_acme-challenge.tts.gsa.gov.external-domains-production.cloud.gov."]
+}
+
+# A record for tts.gsa.gov
 resource "aws_route53_record" "tts_gsa_gov_a" {
   zone_id = aws_route53_zone.tts_gsa_gov_zone.zone_id
   name    = "tts.gsa.gov."
@@ -14,11 +24,52 @@ resource "aws_route53_record" "tts_gsa_gov_a" {
   ttl     = "300"
 }
 
-resource "aws_route53_record" "join_tts_gsa_gov" {
-  zone_id = aws_route53_zone.usability_toplevel.zone_id
+# AAAA for tts.gsa.gov
+#resource "aws_route53_record" "tts_gsa_gov_aaaa" {
+#  zone_id = aws_route53_zone.tts_gsa_gov_zone.zone_id
+#  name    = "tts.gsa.gov."
+#  type    = "AAAA"
+#
+#  alias {
+#    name                   = "TBD"
+#    zone_id                = local.cloud_gov_cloudfront_zone_id
+#    evaluate_target_health = false
+#  }
+#}
+
+# ACME challenge for handbook.tts.gsa.gov
+resource "aws_route53_record" "_acme_challenge_handbook_tts_gsa_gov_cname" {
+  zone_id = aws_route53_zone.tts_gsa_gov_zone.zone_id
+  name    = "_acme-challenge.handbook.tts.gsa.gov."
+  type    = "CNAME"
+  ttl     = 60
+  records = ["5G0wGlMZcqW5HUDxe21M0c4PKziWvZJ336t8f6ZP0-Y"]
+}
+
+# CNAME for handbook.tts.gsa.gov
+resource "aws_route53_record" "handbook_tts_gsa_gov_cname" {
+  zone_id = aws_route53_zone.tts_gsa_gov.zone_id
+  name    = "handbook.tts.gsa.gov."
+  type    = "CNAME"
+  ttl     = 60
+  records = ["d1npw5jx1m5ink.cloudfront.net."]
+}
+
+# ACME challenge for join.tts.gsa.gov
+resource "aws_route53_record" "_acme_challenge_join_tts_gsa_gov_cname" {
+  zone_id = aws_route53_zone.tts_gsa_gov_zone.zone_id
+  name    = "_acme-challenge.join.tts.gsa.gov."
+  type    = "CNAME"
+  ttl     = 60
+  records = ["_acme-challenge.join.tts.gsa.gov.external-domains-production.cloud.gov."]
+}
+
+# CNAME for join.tts.gsa.gov
+resource "aws_route53_record" "join_tts_gsa_gov_cname" {
+  zone_id = aws_route53_zone.tts_gsa_gov.zone_id
   name    = "join.tts.gsa.gov."
   type    = "CNAME"
-  ttl     = 120
+  ttl     = 60
   records = ["d2yv2z3hlrarbd.cloudfront.net."]
 }
 
@@ -32,3 +83,4 @@ module "tts_gov__email_security" {
 output "tts_gsa_gov_ns" {
   value = aws_route53_zone.tts_gsa_gov_zone.name_servers
 }
+
