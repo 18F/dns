7214d37129ce477cea097e8dbda1146134a578c7
Peter Rowland <peter.rowland@gsa.gov>
2023-10-18 10:03:53 -0700

    remove guides.18f.gov A and AAAA records

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -759,30 +759,6 @@ resource "aws_route53_record" "d_18f_gov_grouplet-playbook_18f_gov_aaaa" {
   }
 }
 
-resource "aws_route53_record" "d_18f_gov_guides_18f_gov_a" {
-  zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
-  name    = "guides.18f.gov."
-  type    = "A"
-
-  alias {
-    name                   = "d10jxiv8e4xcp7.cloudfront.net."
-    zone_id                = local.cloud_gov_cloudfront_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "d_18f_gov_guides_18f_gov_aaaa" {
-  zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
-  name    = "guides.18f.gov."
-  type    = "AAAA"
-
-  alias {
-    name                   = "d10jxiv8e4xcp7.cloudfront.net."
-    zone_id                = local.cloud_gov_cloudfront_zone_id
-    evaluate_target_health = false
-  }
-}
-
 # guides.18f.gov — CNAME -------------------------------
 resource "aws_route53_record" "d_18f_gov_guides_18f_gov_cname" {
   zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
