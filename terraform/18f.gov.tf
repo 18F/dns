cf6e51d8449099f9b81b6019eae447c655bc0cfd
Eleni Chappen <eleni.chappen@gsa.gov>
2023-11-16 10:13:07 -0600

    update eng-hiring guide domain

--- terraform/18f.gov.tf
+++ terraform/18f.gov.tf
@@ -597,28 +597,20 @@ resource "aws_route53_record" "d_18f_gov_digitalaccelerator_18f_gov_aaaa" {
   }
 }
 
-resource "aws_route53_record" "d_18f_gov_eng-hiring_18f_gov_a" {
+resource "aws_route53_record" "d_18f_gov__acme_challenge_eng-hiring_18f_gov_cname" {
   zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
-  name    = "eng-hiring.18f.gov."
-  type    = "A"
-
-  alias {
-    name                   = "d1ju28lhpbkq84.cloudfront.net."
-    zone_id                = local.cloud_gov_cloudfront_zone_id
-    evaluate_target_health = false
-  }
+  name    = "_acme-challenge.eng-hiring.18f.gov."
+  type    = "CNAME"
+  ttl     = 120
+  records = ["_acme-challenge.eng-hiring.18f.gov.external-domains-production.cloud.gov."]
 }
 
-resource "aws_route53_record" "d_18f_gov_eng-hiring_18f_gov_aaaa" {
+resource "aws_route53_record" "d_18f_gov_eng-hiring_18f_gov_cname" {
   zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
   name    = "eng-hiring.18f.gov."
-  type    = "AAAA"
-
-  alias {
-    name                   = "d1ju28lhpbkq84.cloudfront.net."
-    zone_id                = local.cloud_gov_cloudfront_zone_id
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  ttl     = 120
+  records = ["eng-hiring.18f.gov.external-domains-production.cloud.gov."]
 }
 
 resource "aws_route53_record" "d_18f_gov_federalist_18f_gov_cname" {
