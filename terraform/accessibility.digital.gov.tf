6e9a473dd098a2c5569ff552de3477dc25a66a60
Nick Lyons <nick.lyons@gsa.gov>
2024-03-20 14:59:54 -0400

    Added DNS record for accessibility.digital.gov

---
+++ terraform/accessibility.digital.gov.tf
@@ -0,0 +1,11 @@
+# redirect accessibility.digital.gov to https://wwww.digital.gov/guides/accessibility-for-teams
+# module "d_18f_gov__join_18f_gov_redirect" {
+#  source  = "mediapop/redirect/aws"
+#  version = "1.2.1"
+#
+#  domains = {
+#    "accessibility.digital.gov " = ["accessibility.digital.gov "]
+#  }
+#
+#  redirect_to = "https://wwww.digital.gov/guides/accessibility-for-teams"
+#}
